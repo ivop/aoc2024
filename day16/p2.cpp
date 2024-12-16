7ae6bd0ae81bd4f46a9b99036a08962f04da34cb
Ivo van Poorten <ivopvp@gmail.com>
2024-12-17 00:33:32 +0100

    Clean up, and faster.

--- day16/p2.cpp
+++ day16/p2.cpp
@@ -10,14 +10,10 @@
 #include <list>
 
 char **map;
-int height, width;
-int space;
+int height, width, space, ypos, xpos, best = INT_MAX;
 
 char *line;
 size_t n;
-
-int ypos, xpos;
-int best = INT_MAX;
 
 enum { NORTH, EAST, SOUTH, WEST };
 
@@ -64,22 +60,16 @@ int main(void) {
     std::list<struct pos> newpos;
     std::list<struct pos> pos;
     std::list<struct pos> reached_end;
-    struct pos start;
-    start.x=xpos;
-    start.y=ypos;
-    start.d=EAST;
-    start.cost = 0;
+    struct pos start = { ypos, xpos, 0, EAST, {} };
     start.path.push_back({ypos,xpos});
     pos.push_back(start);
 
     while (!pos.empty()) {
         for (auto p : pos) {
-            //printf("(%d,%d), ", p.y, p.x);
             if (visited[p.y][p.x] < p.cost - 1000) continue;
             visited[p.y][p.x] = p.cost;
 
             if (map[p.y][p.x] == 'E') {
-                //printf("[[%d]] ", p.cost);
                 if (p.cost < best) best = p.cost;
                 reached_end.push_back(p);
                 continue;
@@ -87,50 +77,48 @@ int main(void) {
 
             int ny, nx, nd;
 
+            p.path.push_back({0,0});
+
             nd = p.d;
             ny = p.y + dirs[nd].dy;
             nx = p.x + dirs[nd].dx;
             if (map[ny][nx] != '#') {
-                std::list<struct coor> path(p.path);
-                path.push_back({ny,nx});
-                newpos.push_back({ny,nx,p.cost+1,nd,path});
+                p.path.pop_back();
+                p.path.push_back({ny,nx});
+                newpos.push_back({ny,nx,p.cost+1,nd,p.path});
             }
 
             nd = (p.d-1)&3;
             ny = p.y + dirs[nd].dy;
             nx = p.x + dirs[nd].dx;
             if (map[ny][nx] != '#') {
-                std::list<struct coor> path(p.path);
-                path.push_back({ny,nx});
-                newpos.push_back({ny,nx,p.cost+1001,nd,path});
+                p.path.pop_back();
+                p.path.push_back({ny,nx});
+                newpos.push_back({ny,nx,p.cost+1001,nd,p.path});
             }
 
             nd = (p.d+1)&3;
             ny = p.y + dirs[nd].dy;
             nx = p.x + dirs[nd].dx;
             if (map[ny][nx] != '#') {
-                std::list<struct coor> path(p.path);
-                path.push_back({ny,nx});
-                newpos.push_back({ny,nx,p.cost+1001,nd,path});
+                p.path.pop_back();
+                p.path.push_back({ny,nx});
+                newpos.push_back({ny,nx,p.cost+1001,nd,p.path});
             }
         }
-        //printf("\n");
         pos.clear();
         pos.swap(newpos);
     }
     printf("part 1: %d\n", best);
 
+    int seats = 0;
     for (auto p : reached_end) {
         if (p.cost != best) continue;
         for (auto c : p.path) {
+            if (map[c.y][c.x] != 'O') seats++;
             map[c.y][c.x] = 'O';
         }
     }
-    int seats = 0;
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {
-            if (map[y][x] == 'O') seats++;
-        }
-    }
+
     printf("part 2: %d\n", seats);
 }
