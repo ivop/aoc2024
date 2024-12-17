5417744b3e28c50efcc34ec98e30014acb34f3ca
Ivo van Poorten <ivopvp@gmail.com>
2024-12-18 00:57:42 +0100

    One line shorter, and no longer init IP with 1

--- day17/p1.c
+++ day17/p1.c
@@ -2,21 +2,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-uint64_t A, B, C, IP = 1, tmp, memtop;
+uint64_t A, B, C, IP, tmp, memtop;
 int mem[1000];
 
 int main(int argc, char **argv) {
     fscanf(stdin, "Register A: %ld\n", &A);
     fscanf(stdin, "Register B: %ld\n", &B);
     fscanf(stdin, "Register C: %ld\n", &C);
-    fscanf(stdin, "Program: %d", &mem[0]);
+    fscanf(stdin, "Program: %d", &mem[IP++]);
     while (fscanf(stdin, ",%d", &mem[IP++])) memtop = IP;
     if (argc != 1) A = strtoll(argv[1],0,10);
     IP = 0;
     while (IP < memtop) {
-        int instr = mem[IP++];
         uint64_t combo[8] = { 0, 1, 2, 3, A, B, C, -1 };
-        switch (instr) {
+        switch (mem[IP++]) {
         case 0: A = A >> combo[mem[IP++]]; break;
         case 1: B = B ^ mem[IP++]; break;
         case 2: B = combo[mem[IP++]] & 7; break;
