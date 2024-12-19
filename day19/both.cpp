3eebded588176884cdb63baf5b9b61638077969a
Ivo van Poorten <ivopvp@gmail.com>
2024-12-20 00:15:06 +0100

    Remove useless includes

--- day19/both.cpp
+++ day19/both.cpp
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <limits.h>
-#include <unistd.h>
 #include <list>
 #include <map>
 #include <stack>
-#include <string>
 
 typedef unsigned long long ull;
 char *line;
